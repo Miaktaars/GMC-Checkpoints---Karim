9bc16094504554bb639174f8066c03d51c495d86
Miaktaars <basta_karim@yahoo.fr>
2025-03-09 14:15:32 +0100

    React Checkpoint 02

---
+++ React-Checkpoint-02/src/App.js
@@ -0,0 +1,14 @@
+import Player from './player';
+import './App.css';
+import ListPlayer from './playerList';
+
+
+function App() {
+  return (
+    <div className="App">
+     <ListPlayer/>
+    </div>
+  );
+}
+
+export default App;
