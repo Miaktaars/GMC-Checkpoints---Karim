4a1848906f13096758f17905bb0c2d6e6781ecdf
Miaktaars <basta_karim@yahoo.fr>
2025-03-10 22:28:57 +0100

    Checkpoint Network Requests in JavaScript

---
+++ Checkpoint-Network-Requests-in-JavaScript/src/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Greeting from "./Greeting";
+import Counter from "./Counter";
+
+function App() {
+  return (
+    <div>
+      <h1>React + TypeScript Example</h1>
+      <Greeting name="John" />
+      <Counter />
+    </div>
+  );
+}
+
+export default App;
